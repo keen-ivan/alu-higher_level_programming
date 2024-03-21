53949d0296651e8901a253136e0cc5a1cc1542a3
keen-ivan <i.mucyo@alustudent.com>
2024-03-21 04:33:16 -0700

    task 7

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
---inserts a new row in the table first_table
-INSERT INTO first_table(id,name) VALUES(89, "Holberton School");
+-- insert data into the table --
+INSERT INTO `first_table` VALUES (89, "Best School");
