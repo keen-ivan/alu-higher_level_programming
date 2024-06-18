37107bbdb28b192672e244d7a741d09ae8bd12f0
keen-ivan <i.mucyo@alustudent.com>
2024-06-18 18:33:23 +0200

    all

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let message1 = "C is fun";
+let message2 = "python is cool";
+let message3 = "JavaScript is amazing";
+console.log(message1);
+console.log(message2);
+console.log(message3);
